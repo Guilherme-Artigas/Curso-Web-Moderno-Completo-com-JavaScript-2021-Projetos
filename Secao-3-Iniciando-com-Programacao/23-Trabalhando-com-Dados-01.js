2b9a3a3c110a1fab4990d81ceac68a544e946e23
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-03-10 11:56:49 -0300

    Seção 3 Aula 23

---
+++ Secao-3-Iniciando-com-Programacao/23-Trabalhando-com-Dados-01.js
@@ -0,0 +1,11 @@
+var nome = 'Caneta'; // String / Frase / Letras
+var quantidade = 10; // Númérico inteiro
+var preco = 6.4; // Númérico flutuante
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+
+console.log(nome); 
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
