b656986628ed0bc40a8e9047e3165ba5f7290ddd
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-03-11 11:35:04 -0300

    Seção 3  Aula 24

---
+++ Secao-3-Iniciando-com-Programacao/24-Trabalhando-com-Dados-02.js
@@ -0,0 +1,9 @@
+var preco = 19.90;
+var desconto = 0.4;
+var PrecoComDesconto = preco * (1 - desconto);
+console.log(PrecoComDesconto);
+
+var nome = 'Caderno';
+var categoria = 'Papelaria';
+
+console.log(nome + ' ' + categoria);
