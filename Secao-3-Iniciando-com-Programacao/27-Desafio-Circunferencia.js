c876dabec988a4ceb18d70ce89a17e0199e2a5a2
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-03-14 10:54:31 -0300

    Seção 3 Desafio 27

---
+++ Secao-3-Iniciando-com-Programacao/27-Desafio-Circunferencia.js
@@ -0,0 +1,8 @@
+/*
+    Desafio Área da circunferência
+*/
+
+const PI = 3.1415;
+var raio = 10;
+var area = PI * raio * raio;
+console.log('O resultado da área é ' + area + 'm²');
---
+++ Secao-3-Iniciando-com-Programacao/28-Desafio-Area-da-Circunferencia-Resposta.js
@@ -0,0 +1,8 @@
+/*
+    Desafio Área da Circunferencia - Resposta
+*/
+
+const PI = 3.141592;
+const raio = 10;
+const area = PI * raio * raio;
+console.log('O valor da área é ' + area + 'm².')
