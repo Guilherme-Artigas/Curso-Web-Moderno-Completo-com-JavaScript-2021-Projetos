9602fd577ec4316f7ccdaef47ab105fabddd2b91
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-03-16 09:45:02 -0300

    Seção 4 Aula 35

---
+++ Secao-4-JavaScript-Fundamentos/35-Organizacao-Basica-de-um-CodigoJS.js
@@ -0,0 +1,9 @@
+console.log('Sentença de código.');
+
+{
+    console.log('1º Conjunto de sentenças de código agrupadas em um bloco.');
+    console.log('2º Conjunto de sentenças de código agrupadas em um bloco.');
+    console.log('3º Conjunto de sentenças de código agrupadas em um bloco.');
+    console.log('4º Conjunto de sentenças de código agrupadas em um bloco.');
+    console.log('5º Conjunto de sentenças de código agrupadas em um bloco.');
+}
