413f15d9fb43d3fdbcbea4b7e85f4abbf5269070
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-03-17 09:48:24 -0300

    Seção 4 Aula 38

---
+++ Secao-4-JavaScript-Fundamentos/38-O-Basico-de-var-let-const.js
@@ -0,0 +1,8 @@
+// var exemplo_com_var;
+// let exemplo_com_let;
+// const exemplo_com_const;
+
+/*
+ * Se declararmos um identificador ele deve ser obrigatoriamente preenchido com valor,
+ * caso contrario o script reclama alegando erro.
+ */
