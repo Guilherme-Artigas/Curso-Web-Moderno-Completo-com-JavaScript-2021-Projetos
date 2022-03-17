ed495a6113fff6dd697d7911bfa7721d7eb64fc0
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-03-17 15:11:55 -0300

    Seção 4 Aula 40

---
+++ Secao-4-JavaScript-Fundamentos/40-Tipos-em-JS-Number.js
@@ -0,0 +1,18 @@
+const peso1 = 1.0;
+const peso2 = Number('2.0'); // A string é convertida em número.
+
+console.log(peso1, peso2);
+console.log(Number.isInteger(peso1));
+console.log(Number.isInteger(peso2));
+
+const avaliacao1 = 9.871;
+const avaliacao2 = 6.871;
+
+const total = avaliacao1 * peso1 + avaliacao2 * peso2;
+const media = total / (peso1 + peso2);
+
+console.log(media.toFixed(1)); // Controle de casas decimais com arredondamento para cima.
+console.log(media.toString()); // Conversão de tipo, media é numérico, com essa sentença fazemos a conversão para string. Se passarmos o número 2 como argumento dentro de .toString(2) descobriremos o valor de determinado número em sequencia bínaria.
+
+let media_texto = media.toString();
+console.log(typeof media_texto);
