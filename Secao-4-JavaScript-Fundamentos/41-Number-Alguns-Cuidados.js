cf347ca2b98e915a54ffa45f954d15ac9df3bfbd
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-03-18 09:08:12 -0300

    Seção 4 Aula 41

---
+++ Secao-4-JavaScript-Fundamentos/41-Number-Alguns-Cuidados.js
@@ -0,0 +1,12 @@
+let a = '10';
+let b = 2;
+let c = a / 2;
+
+console.log(c);
+console.log(typeof c); // variável c será considerada do tipo Number
+
+console.log(7 / 0);
+console.log('50' / 2);
+console.log('Show!' * 2);
+// console.log(10.toString()); ira dar erro.
+console.log((10).toString()); // Colocando o valor literal entre parenteses, tudo ira ficar bem.
