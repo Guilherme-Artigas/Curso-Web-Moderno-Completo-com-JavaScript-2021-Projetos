42f2e12d634ec7533f6b84ee8b89dcb7e1178f2d
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-03-22 14:38:36 -0300

    Seção 4 Aula 44

---
+++ Secao-4-JavaScript-Fundamentos/44-Usando-Template-Strings.js
@@ -0,0 +1,9 @@
+let nome = 'Guilherme Artigas';
+
+console.log(`Olá sem bem vindo ao nosso atendimento, ${nome}`);
+
+console.log(`Soma entre os valores 5 e 7 é ${soma(5, 7)}`);
+
+function soma(parametro1, parametro2) {
+    return parametro1 + parametro2;
+}
