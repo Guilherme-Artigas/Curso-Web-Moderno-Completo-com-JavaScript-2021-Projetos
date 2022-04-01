2c3778a1a5af97f59f19d0f6a8ed4fa42e126660
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-04-01 14:40:05 -0300

    Seção 4 aula 53

--- Secao-4-JavaScript-Fundamentos/52-Declaracoes-com-Var-01.js
+++ Secao-4-JavaScript-Fundamentos/52-Declaracoes-com-Var-01.js
@@ -16,3 +16,11 @@ function local() {
 };
 
 console.log(escopo_local); // Retorna erro, pois, foi declarada dentro de function.
+
+/*
+    Declarações de variáveis com a palavra reservada var tem o escopo global dentro de qualquer bloco de código, ou seja, é vista por todo o código podendo ser chamada a qualquer momento sem retornar erro, a exceção e se var estiver dentro de um bloco associado a uma função, nesse cenário o escopo de var não é mais global e sim local.
+
+    Por que devemos fugir de declarações de variáveis com escopo global?
+
+    Existe a vantagem de poder acessar a variável global de qualquer lugar do sistema, porém, qualquer sobrescrita dessa variável global pode causar uma pane geral no sistema.
+*/
