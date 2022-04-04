70af9b881d0336695567809080052aefcf7a16fe
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-04-04 10:49:13 -0300

    Seção 4 Aula 59

---
+++ Secao-4-JavaScript-Fundamentos/59-Entendendo-Hoisting.js
@@ -0,0 +1,9 @@
+console.log(`a = ${a}`);
+var a = 2;
+console.log(`a = ${a}`);
+
+// Hoisting funciona somente com var.
+
+console.log(`b = ${b}`); // Vai dar erro, Hoisting funciona somente com var.
+let b = 4;
+console.log(`b = ${b}`);
