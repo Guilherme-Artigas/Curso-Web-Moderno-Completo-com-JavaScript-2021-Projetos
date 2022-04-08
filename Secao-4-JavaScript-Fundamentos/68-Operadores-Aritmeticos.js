fa7319146cfed6dec56deb213f228de85cc85bb3
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-04-08 12:40:37 -0300

    Seção 4 Aula 68

---
+++ Secao-4-JavaScript-Fundamentos/68-Operadores-Aritmeticos.js
@@ -0,0 +1,13 @@
+const [a, b, c, d] = [5, 12, 1, 26];
+
+const soma = b + d; // Soma
+const subt = a - b; // Subtração
+const mult = c * d; // Multiplicação
+const divi = d / a; // Divisão
+const modu = d % 2; // Módulo (resto da divisão)
+
+console.log(soma); // 38
+console.log(subt); // -7
+console.log(mult); // 26
+console.log(divi); // 5.2
+console.log(modu); // 0
