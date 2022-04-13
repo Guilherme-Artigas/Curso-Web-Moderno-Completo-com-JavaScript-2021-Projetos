70849bace8d710622f3e13c65b7abf1354785442
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-04-13 11:06:00 -0300

    Seção 5 Aula 82

--- Secao-5-JavaScript-Estruturas-de-Controle/82_Usando_a_Estrutura_While.js
+++ Secao-5-JavaScript-Estruturas-de-Controle/82_Usando_a_Estrutura_While.js
@@ -11,3 +11,5 @@ while (opcao != -1) {
 };
 
 console.log(`Saiu do Laço!`);
+
+// Uso adequado do while: é baseado em uma expressão verdadeira ou falsa, enquanto for verdadeiro continua e quando se tornar falso o laço para de ser executado, ideal para ser usado em situações em que se tem uma repetição indeterminada de vezes.
