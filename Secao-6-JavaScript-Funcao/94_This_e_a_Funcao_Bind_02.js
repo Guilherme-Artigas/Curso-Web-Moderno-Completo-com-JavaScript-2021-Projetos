c54d6f2f790fbd6118fa81c8a7672917c1e374ef
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-04-15 13:38:41 -0300

    Seção 6 Aula 94

---
+++ Secao-6-JavaScript-Funcao/94_This_e_a_Funcao_Bind_02.js
@@ -0,0 +1,13 @@
+// Forma de driblar o this que varia.
+
+function Pessoa () {
+    this.idade = 0;
+
+    const self = this;
+    setInterval(function () {
+        self.idade++
+        console.log(self.idade);
+    }/*.bind(this)*/, 1000);
+};
+
+new Pessoa;
