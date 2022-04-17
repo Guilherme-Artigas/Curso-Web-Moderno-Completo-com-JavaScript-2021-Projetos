b291b4a10439fcdfe3b3f8f4965a018b56bc09c7
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-04-17 13:49:01 -0300

    Testando callbacks

---
+++ Testando_Callbacks.js
@@ -0,0 +1,11 @@
+console.log(`Digite seu nome: `);
+
+function pega_Nome (p1) {
+    return p1;
+};
+
+function saudacao (p1) {
+    console.log(`Bom dia / Tarde / Noite, Sr(a) ${p1}`);
+};
+
+saudacao(pega_Nome('Guilherme Artigas'));
