efc49e9c4d2b07834685606ba35e837593d7b3af
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-04-20 10:46:50 -0300

    Ex_03

---
+++ Secao-6-JavaScript-Funcao/113_Lista_Exercicios_Secao_4_5_6/Ex_03.js
@@ -0,0 +1,9 @@
+/*
+    03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+*/
+
+function exponenciacao (p1, p2) {
+    return console.log(p1 ** p2);
+};
+
+exponenciacao(2, 3);
