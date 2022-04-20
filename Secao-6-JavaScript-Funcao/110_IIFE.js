8813a3988b3a87b152dc3e4b87c76f26be754601
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-04-20 09:14:37 -0300

    Seção 6 Aula 110

---
+++ Secao-6-JavaScript-Funcao/110_IIFE.js
@@ -0,0 +1,15 @@
+// Exemplo 1 - Glossário MDN
+(function () {
+    var nome = 'Guilherme';
+})();
+
+// Exemplo 2 - Glossário MDN
+let resultado = (function () {
+    let nome = 'Artigas';
+    return nome;
+})();
+
+console.log(resultado);
+
+/*--------------------------------------------------------------------------------------------*/
+
