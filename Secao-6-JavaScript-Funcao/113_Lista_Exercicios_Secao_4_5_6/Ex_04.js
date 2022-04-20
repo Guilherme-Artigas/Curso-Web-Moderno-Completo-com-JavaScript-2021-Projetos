a2cb01f2b35d1e231da60930556980777eb8ef91
Guilherme Artigas <Guilherme.Artigas92@gmail.com>
2022-04-20 10:49:45 -0300

    Ex_04

---
+++ Secao-6-JavaScript-Funcao/113_Lista_Exercicios_Secao_4_5_6/Ex_04.js
@@ -0,0 +1,9 @@
+/*
+    04) Crie uma função que irá receber dois valores, o dividendo e o divisor. A função deverá imprimir o resultado e o resto da divisão destes dois valores.
+*/
+
+function dividir (p1, p2) {
+    return console.log(`Resultado da divisão: ${p1 / p2}, Resto da divisão: ${p1 % p2}`);
+};
+
+dividir(20, 3);
