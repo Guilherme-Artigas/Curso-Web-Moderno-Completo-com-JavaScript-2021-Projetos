42102429c4e475cae47a30e5447c7ad09af14c32
Guilherme <guilherme.artigas92@gmail.com>
2022-04-27 13:41:11 -0300

    Ex_25

---
+++ Secao-6-JavaScript-Funcao/113_Lista_Exercicios_Secao_4_5_6/Ex_25.js
@@ -0,0 +1,13 @@
+/*
+    25) ​Escrever um programa para exibir os números de 1 até 50 na tela.
+*/
+
+function contando () {
+    let indice = 0;
+    do {
+        console.log(indice);
+        indice++;
+    } while (indice <= 50);
+};
+
+contando();
