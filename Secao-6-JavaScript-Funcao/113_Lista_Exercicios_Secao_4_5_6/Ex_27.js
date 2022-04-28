d947e760a2e9e65e865b6f59181adb6cb02a959d
Guilherme A-Ubuntu <guilherme.artigas92@gmail.com>
2022-04-28 11:10:51 -0300

    Ex_27

--- Secao-6-JavaScript-Funcao/113_Lista_Exercicios_Secao_4_5_6/Ex_27.js
+++ Secao-6-JavaScript-Funcao/113_Lista_Exercicios_Secao_4_5_6/Ex_27.js
@@ -3,55 +3,41 @@
 */
 
 function taxaCrescimento (altura1, taxaC1, altura2, taxaC2) {
-    let menor = null;
     let dTaxa = null;
-    let dAltu = null;
-    let indic = null;
-    if (altura1 < altura2) {
-        menor = altura1;
-    } else if (altura2 < altura1) {
-        menor = altura2;
-    };
-
-    if (taxaC1 != taxaC2) {
+    let indic = 1;
+    if (altura1 > altura2 && taxaC1 < taxaC2) {
+        // significa que a criança2 vai passar a criança1 em altura.
         if (taxaC1 > taxaC2) {
             dTaxa = taxaC1 - taxaC2;
         } else if (taxaC2 > taxaC1) {
             dTaxa = taxaC2 - taxaC1;
         };
 
-        if (altura1 > altura2) {
-            dAltu = altura1 - altura2;
-            while (altura1 >= altura2) {
-                altura2 += dTaxa;
-                indic += 1;
-            };
-            return console.log(`Criança 1 é maior que a criança 2, tem taxas de crescimento diferentes! Criança 2 passara a criança 1 em ${indic} anos`);
-        } else if (altura2 > altura1) {
-            dAltu = altura2 - altura1;
-            while (altura2 >= altura1) {
-                altura1 += dTaxa;
-                indic += 1;
-            };
-            return console.log(`Criança 2 é maior que a criança 1, tem taxas de crescimento diferentes! Criança 1 passara a criança 2 em ${indic} anos`);
+        while (altura2 <= altura1) {
+            console.log(`Quantidade de tempo em ano(s): ${indic}`);
+            altura2 += dTaxa;
+            indic += 1;
+        };
+    };
+
+    if (altura1 < altura2 && taxaC1 > taxaC2) {
+        // significa que a criança1 vai passar a criança2 em altura.
+        if (taxaC1 > taxaC2) {
+            dTaxa = taxaC1 - taxaC2;
+        } else if (taxaC2 > taxaC1) {
+            dTaxa = taxaC2 - taxaC1;
         };
 
-    } else {
-        console.log(`As crianças tem a mesma taxa de crescimento, portanto é impossível de uma passar a outra em altura!`);
+        while (altura1 <= altura2) {
+            console.log(`Quantidade de tempo em ano(s): ${indic}`);
+            altura1 += dTaxa;
+            indic += 1;
+        };
     };
-    //return console.log(`${indic}`);
+
+    if (taxaC1 == taxaC2) {
+        console.log(`É impossivel uma criança passar a outra em altura, pois as duas possuem a mesma taxa de crescimento anual.`)
+    };
 };
 
-taxaCrescimento(150, 2, 130, 4);
-// 152, 134
-// 154, 138
-// 156, 142
-// 158, 146
-// 160, 150
-// 162, 154
-// 164, 158
-// 166, 162
-// 168, 166
-// 170, 170
-// 172, 174, precisou de 11 anos
-// 
+taxaCrescimento(185, 2, 200, 2);
