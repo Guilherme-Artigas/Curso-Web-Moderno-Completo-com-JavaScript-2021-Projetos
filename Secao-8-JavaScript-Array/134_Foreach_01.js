9ea7d19cd85bfaa631916280641b96290fd537f0
Guilherme Artigas Ubuntu <guilherme.artigas92@gmail.com>
2022-05-12 10:29:40 -0300

    Seção 8 Aula 134

---
+++ Secao-8-JavaScript-Array/134_Foreach_01.js
@@ -0,0 +1,12 @@
+const aprovados = ['Guilherme', 'Rafaela', 'Bryan', 'Julia'];
+
+aprovados.forEach(function (nome, indice) {
+    console.log(`${indice + 1}) ${nome}`);
+});
+console.log(`---------------------------------------------`);
+
+aprovados.forEach((nome, indice) => console.log(indice + 1 + '} ' + nome));
+console.log(`---------------------------------------------`);
+
+const exibirAprovados = (nome, indice) => console.log(indice + 1 + '] ' + nome);
+aprovados.forEach(exibirAprovados);
