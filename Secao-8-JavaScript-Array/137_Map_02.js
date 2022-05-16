0015b2d3bd411f130edf35402a09b7bb9558c570
Guilherme Artigas Ubuntu <guilherme.artigas92@gmail.com>
2022-05-16 11:14:31 -0300

    Seção 8 Aula 137

---
+++ Secao-8-JavaScript-Array/137_Map_02.js
@@ -0,0 +1,12 @@
+const carrinhoDeCompras = [
+    '{ "nome": "Borracha", "preco": 3.45 }',
+    '{ "nome": "Caderno", "preco": 13.90 }',
+    '{ "nome": "Kit de Lapis", "preco": 41.22 }',
+    '{ "nome": "Caneta", "preco": 7.50 }',
+];
+
+const paraObjeto = json => JSON.parse(json);
+const apenasPreco = produto => produto.preco;
+
+const resultado = carrinhoDeCompras.map(paraObjeto).map(apenasPreco);
+console.log(resultado);
