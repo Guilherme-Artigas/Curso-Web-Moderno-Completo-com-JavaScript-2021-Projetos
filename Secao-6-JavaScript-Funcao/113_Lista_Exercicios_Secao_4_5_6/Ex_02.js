3f76eb5387ceaea34995d8e29d5ec20748859a8d
Guilherme Artigas Ubuntu <guilherme.artigas92@gmail.com>
2022-05-27 08:36:30 -0300

    Seção 6 Ex_02

--- Secao-6-JavaScript-Funcao/113_Lista_Exercicios_Secao_4_5_6/Ex_02.js
+++ Secao-6-JavaScript-Funcao/113_Lista_Exercicios_Secao_4_5_6/Ex_02.js
@@ -13,3 +13,19 @@ function formato_Triangulo (p1, p2, p3) {
 };
 
 formato_Triangulo(4, 5, 6);
+
+// Versão da resolução desse exercício utilizando arrow function
+
+const formato_Triangulo2 = (lado1, lado2, lado3) => {
+    let formato = null;
+    if (lado1 === lado2 && lado2 === lado3 && lado1 === lado3) {
+        formato = 'Equilátero';
+    } else if (lado1 === lado2 || lado2 === lado3 || lado1 === lado3) {
+        formato = 'Isóceles';
+    } else if (lado1 !== lado2 && lado2 !== lado3 && lado1 !== lado3) {
+        formato = 'Escaleno';
+    };
+    return console.log(`O formato do triângulo com os valores passados, resultou em um triângulo ${formato}`);
+};
+
+formato_Triangulo2(3, 6, 6);
